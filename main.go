8a41254cf445e8fb046d2c0d5328f2f73c9054cd
misha <mike.kozhevnikov03@gmail.com>
2025-03-01 22:15:55 +0300

    Добавлен движок команддной строки, добавлен README

--- main.go
+++ main.go
@@ -1,5 +1,52 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"manager/console"
+	"os"
+	"strings"
+)
 
+var commands_list = []string{"help", "exit"}
+var commands_descriptions = []string{
+	"Выводит список команд",
+	"Выход из программы",
+}
+
+func main() {
+	console.ClearScreen()
+	for {
+		command, _ := GetCommand()
+
+		switch command {
+		case commands_list[0]:
+			PrintHelp(commands_list, commands_descriptions)
+		case commands_list[1]:
+			return
+		}
+	}
+}
+
+func GetCommand() (string, error) {
+	var command string
+	scanner := bufio.NewScanner(os.Stdin)
+
+	if scanner.Scan() {
+		command = scanner.Text()
+	}
+	if err := scanner.Err(); err != nil {
+		return "", errors.New("command input error")
+	}
+
+	return strings.TrimSpace(command), nil
+}
+
+func PrintHelp(commands_list []string, commands_description []string) {
+	fmt.Println()
+	for i, elem := range commands_list {
+		fmt.Printf("%v - %v\n", elem, commands_description[i])
+	}
+	fmt.Println()
 }
